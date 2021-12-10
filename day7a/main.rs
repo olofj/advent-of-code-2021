0cd1a3b4a9749f3d1068d4474362f7a67e7e0344
Olof Johansson <olof@lixom.net>
2021-12-09 18:42:04 -0800

    Add back missing day 7

---
+++ day7a/main.rs
@@ -0,0 +1,30 @@
+fn diff(a: usize, b:usize) -> usize {
+    let dist = if a > b {
+        a - b
+    } else {
+        b - a
+    };
+    dist
+}
+
+fn main() {
+    let input: Vec<usize> = include_str!("input.txt")
+//    let input: Vec<usize> = "16,1,2,0,4,2,7,1,2,14"
+        .trim_end()
+        .split(",")
+        .map(|c| c.parse::<usize>().unwrap())
+        .collect();
+
+    let &max = input.iter().max().unwrap();
+    let mut costs: Vec<usize> = (0..=max).collect();
+
+    for loc in 0..=max {
+        costs[loc] = 0;
+        for &i in input.iter() {
+            costs[loc] += diff(i,loc);
+        }
+    }
+
+    println!("costs: {:?}", costs);
+    println!("cheapest: {}", costs.iter().min().unwrap());
+}
