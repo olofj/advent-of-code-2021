cb9bafdbcaf3248ea4340e6a18e8b152bcd84ba3
Olof Johansson <olof@lixom.net>
2021-12-10 07:58:34 -0800

    day9b cleanups and improvements

--- day9b/main.rs
+++ day9b/main.rs
@@ -1,41 +1,38 @@
 const NAV: [(isize, isize); 4] = [(-1, 0), (0, -1), (1, 0), (0, 1)];
 
-type Color = u32;
-const NO_COLOR: Color = 0 as Color;
-const XSIZE: usize = 100;
-const YSIZE: usize = 100;
-const MAX_COLOR: usize = (XSIZE/2)*(YSIZE/2);
+type Color = isize;
+const NO_COLOR: Color = -1 as Color;
+const PEAK: Color = 0 as Color;
 
-fn fill(map: &Vec<Vec<isize>>,
-        fill: &mut [[Color; YSIZE]; XSIZE],
-        (startx, starty): (usize, usize),
-        color: Color) {
-    let mut q:Vec<(usize,usize)> = Vec::new();
-    if fill[startx][starty] == NO_COLOR &&
-        map[startx][starty] != 9 {
-        fill[startx][starty] = color;
+fn fill(map: &Vec<Vec<Color>>, (startx, starty): (usize, usize)) -> Vec<(usize, usize)> {
+    let mut q: Vec<(usize,usize)> = Vec::new();
+    let mut ret: Vec<(usize, usize)> = Vec::new();
+
+    if map[startx][starty] == NO_COLOR {
+        ret.push((startx, starty));
         q.push((startx, starty));
     }
 
     while let Some((x,y)) = q.pop() {
-        for (dx, dy) in NAV {
+        'nav: for (dx, dy) in NAV {
             let xx = x as isize + dx;
             let yy = y as isize + dy;
 
-            if xx < 0 || xx >= XSIZE as isize {
-                continue;
-            }
-            if yy < 0 || yy >= YSIZE as isize {
-                continue;
+            if xx < 0 || xx >= map.len() as isize {
+                continue 'nav;
             }
             let xx = xx as usize;
+            if yy < 0 || yy >= map[xx].len() as isize {
+                continue 'nav;
+            }
             let yy = yy as usize;
-            if map[xx][yy] != 9 && fill[xx][yy] == NO_COLOR {
-                fill[xx][yy] = color;
+            if map[xx][yy] != PEAK && ! ret.contains(&(xx,yy)) {
+                ret.push((xx,yy));
                 q.push((xx,yy));
             }
         }
     }
+    ret
 }
 
 fn main() {
@@ -44,30 +41,43 @@ fn main() {
         .map(|l| l.chars().map(|c| (c as isize) - ('0' as isize)).collect())
         .collect();
 
-    let plot = &mut [[NO_COLOR; YSIZE]; XSIZE];
+    let mut map: Vec<Vec<Color>> = input
+        .iter()
+        .map(|l| l
+             .iter()
+             .map(|c| match c { 9 => PEAK, _ => NO_COLOR })
+             .collect())
+        .collect();
+
     let mut color = 1 as Color;
-    for x in 0..XSIZE {
-        for y in 0..YSIZE {
-            if plot[x][y] == NO_COLOR && input[x][y] != 9 {
-                fill(&input, plot, (x, y), color);
+
+    for x in 0..map.len() {
+        for y in 0..map[x].len() {
+            let c = map[x][y];
+            if c == NO_COLOR {
+                for (fx, fy) in fill(&map, (x, y)) {
+                    map[fx][fy] = color;
+                }
                 color += 1;
             }
         }
     }
 
-    let mut hist = [0; MAX_COLOR];
+    let mut hist: Vec<usize> = vec![0; color as usize];
 
-    for x in 0..XSIZE {
-        for y in 0..YSIZE {
-            let color = plot[x][y];
-            if color != NO_COLOR {
+    for x in 0..map.len() {
+        for y in 0..map[x].len() {
+            let color = map[x][y];
+            if color == NO_COLOR {
+                panic!("Never filled ({},{})", x, y);
+            }
+            if color != PEAK {
                 hist[color as usize] += 1;
             }
         }
     }
 
-    let mut hvec: Vec<(usize, &usize)> = hist.iter().enumerate().collect();
-    hvec.sort_by(|(_, av), (_, bv)| bv.cmp(av));
+    hist.sort_by(|a, b| b.cmp(a));
 
-    println!("product {}", hvec[0].1 * hvec[1].1 * hvec[2].1);
+    println!("product {}", hist.iter().take(3).product::<usize>());
 }
