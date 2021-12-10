0d1dcd3e7696e010187f0b8fb1365a72dd9dd8ce
Olof Johansson <olof@lixom.net>
2021-12-09 19:28:42 -0800

    recovered badly renamed day5b

--- day5b/main.rs
+++ day5b/main.rs
@@ -1,31 +1,53 @@
-fn main() {
-    let mut timer = [0u64; 9];
 
-    let input: Vec<usize> = include_str!("input.txt")
-        .trim_end()
-        .split(",")
-        .map(|c| c.parse::<usize>().unwrap())
-        .collect();
+use std::cmp::max;
 
-    for &f in input.iter() {
-        timer[f] += 1;
+fn abs_diff(a: usize, b: usize) -> usize {
+    if a > b {
+        a-b
+    } else {
+        b-a
     }
+}
 
-    println!("start {:?}", timer);
-    for d in 1..=80 {
-        let new = timer[0];
-        for t in 0..8 {
-            timer[t] = timer[t+1];
+fn range(a: usize, b: usize, len: usize) -> Vec<usize> {
+    let mut r: Vec<usize> = if a <= b {
+        (a..=b).collect()
+    } else {
+        (b..=a).rev().collect()
+    };
+    r.resize(len, a);
+    r
+}
+
+fn main() {
+    let mut board = [[0u32; 1024]; 1024];
+
+    let input: Vec<&str> = include_str!("input.txt").lines().collect();
+
+    for l in input {
+        let points = l.split_once(" -> ").unwrap();
+        let (x1, y1) = points.0.split_once(",").unwrap();
+        let (x2, y2) = points.1.split_once(",").unwrap();
+        let (x1, y1) = (x1.parse::<usize>().unwrap(), y1.parse::<usize>().unwrap());
+        let (x2, y2) = (x2.parse::<usize>().unwrap(), y2.parse::<usize>().unwrap());
+
+        let maxlen = max(abs_diff(x1, x2), abs_diff(y1, y2)) + 1;
+
+        let xrange = range(x1, x2, maxlen);
+        let yrange = range(y1, y2, maxlen);
+
+        for (&x,y) in xrange.iter().zip(yrange) {
+            board[x][y] += 1;
         }
-        timer[6] += new;
-        timer[8] = new;
-        println!("After day {}: {:?}", d, timer);
     }
 
     let mut sum = 0;
-    for t in 0..=8 {
-        sum += timer[t];
+    for i in 0..1024 {
+        for j in 0..1024 {
+            if board[i][j] > 1 {
+                sum += 1;
+            }
+        }
     }
-
-    println!("Sum is {}", sum);
+    println!("sum {}", sum);
 }
